4a8404c94c2265953cf829054e2be2d85bf09972
AntonioNDev <antonionikoloski2003@gmail.com>
2022-12-19 18:00:00 +0100

    V1

--- appGUI.py
+++ appGUI.py
@@ -1,23 +1,26 @@
 from tkinter import *
+from tkinter import ttk
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import sqlite3 as sql
 
 window = Tk()
-window.title("Battery App GUI")
+window.title("Battery Info")
 
-conn = sql.connect('C:/Users/Antonio/Documents/MyProjects/Battery/database.db')
+databasePath = 'C:/Users/Antonio/Documents/MyProjects/BatteryInfo/database.db'
+
+conn = sql.connect(databasePath)
 cur = conn.cursor()
 
 class AppGUI:
    def __init__(self):
-      self.appWidth = 900
-      self.appHeight = 700
+      self.appWidth = 1250
+      self.appHeight = 750
       self.screen_w = window.winfo_screenwidth()
       self.screen_h = window.winfo_screenheight()
 
-      x = (self.screen_w / 2) - (self.appWidth) + 450
+      x = (self.screen_w / 2) - (self.appWidth) + 600
       y = (self.screen_h / 2) - (self.appHeight) + 350
       window.geometry(f'{self.appWidth}x{self.appHeight}+{int(x)}+{int(y)}')
 
@@ -39,7 +42,7 @@ class AppGUI:
                numz2 = yPointIndices[pairs[i+1]]
                calc.append(abs(time[numz1]-time[numz2]))
 
-      result = sum(calc) / len(calc) + 0.9
+      result = sum(calc) / len(calc) + 1
 
       return result
 
@@ -82,7 +85,7 @@ class AppGUI:
       return batteryChargedToday
 
    def createGraph(self, x, y):
-      f = plt.Figure(figsize=(11.5, 5.7), dpi=80)
+      f = plt.Figure(figsize=(10, 5), dpi=90)
       f.add_subplot().plot(x, y)
       
       canvas = FigureCanvasTkAgg(f, bottomFrame)
@@ -111,17 +114,15 @@ class AppGUI:
                yDataFAvg.append(x[0])
                xDataFAvg.append(hours)
 
-
             self.createGraph(Xpoints, Ypoints)
 
             batteryCharg = self.batteryCharged(Ypoints)
             averBatt = self.avgBattLife(xDataFAvg, yDataFAvg)
 
             timesChargedLabel.config(text=f"Battery Charged: {batteryCharg} time/s.") #Labels for how many times batt was charged and average life of the battery
-            avgBattLife.config(text=f"Battery was charged every: ≈{averBatt:.1f}h")
+            avgBattLife.config(text=f"Average battery life: ≈{averBatt:.1f}h")
 
             errorLabel.config(text="")
-
 
          except Exception as e:
             print(e)
@@ -130,31 +131,63 @@ class AppGUI:
       else:
          errorLabel.config(text="Empty inputs!.")
 
+   def searchQuery(self, month, year):
+      my_tree.delete(*my_tree.get_children())
+      if month and year:
+         try:
+            data = conn.execute(f"SELECT * FROM {month} WHERE year=?;", (int(year),)).fetchall()
+            previusDate = ''
+
+            for i, x in enumerate(data):
+               if(x[1] != previusDate):
+                  my_tree.insert(parent='', index='end', text='', values=(x[1], x[3]))
+
+               previusDate = x[1]
+
+            errorLabel.config(text="")
+            dataFrame.configure(text=f'Data for: {month}')
+
+         except Exception as e:
+            errorLabel.config(text="No data for that month/day/year. Try again!.")
+
+      else:
+         errorLabel.config(text="Empty inputs!.")
+
+
    def main(self):
-      global errorLabel, bottomFrame, timesChargedLabel, avgBattLife
+      global errorLabel, bottomFrame, timesChargedLabel, avgBattLife, my_tree, dataFrame
 
-      mainFrame = Frame(window, relief='sunken', bg='red', height=500)
-      mainFrame.pack(fill=BOTH)
+      sideFrame = Frame(window, relief='sunken', height=self.appHeight, width=350, border=3)
+      sideFrame.pack(side=LEFT)
 
-      topFrame = Frame(mainFrame, relief='sunken', border=3, height=250, bg='#81b29a')
-      topFrame.grid(row=0, column=0, ipadx=self.appWidth)
+      searchFrame = LabelFrame(sideFrame, height=130, width=350, bg='#ccd5ae', text='Month & Year')
+      searchFrame.grid(row=0, column=0, sticky='n')
+
+      dataFrame = LabelFrame(sideFrame, height=600, width=350, bg='#e5e5e5', relief='sunken', border=2, text=f'')
+      dataFrame.grid(row=1, column=0)
+      
+      mainFrame = Frame(window, relief='sunken', height=500, width=1000)
+      mainFrame.pack(side=RIGHT)
+
+      topFrame = Frame(mainFrame, relief='sunken', border=3, height=250, bg='#81b29a', width=900)
+      topFrame.grid(row=0, column=1)
 
       ########## top frame inputs and labels ###########
-      monthLabel = Label(topFrame, text='Month', font=('Lucida Bright', 18), bg='#81b29a')
+      monthLabel = Label(topFrame, text='Month', font=('Lucida Bright', 17), bg='#81b29a')
       monthLabel.grid(row=0, column=0, padx=115)
 
       monthEntry = Entry(topFrame, highlightthickness=1, border=2, font=('Lucida Bright', 14), justify=CENTER)
       monthEntry.grid(row=1, column=0)
 
       ####
-      dayLabel = Label(topFrame, text='Day/Date', font=('Lucida Bright', 18), bg='#81b29a')
+      dayLabel = Label(topFrame, text='Day/Date', font=('Lucida Bright', 17), bg='#81b29a')
       dayLabel.grid(row=0, column=1, padx=30, pady=30)
 
       dayEntry = Entry(topFrame, highlightthickness=1, border=2, font=('Lucida Bright', 14), justify=CENTER)
       dayEntry.grid(row=1, column=1)
 
       ####
-      yearLabel = Label(topFrame, text='Year', font=('Lucida Bright', 18), bg='#81b29a')
+      yearLabel = Label(topFrame, text='Year', font=('Lucida Bright', 17), bg='#81b29a')
       yearLabel.grid(row=0, column=2, padx=130, pady=30)
 
       yearEntry = Entry(topFrame, highlightthickness=1, border=2, font=('Lucida Bright', 14), justify=CENTER)
@@ -163,8 +196,8 @@ class AppGUI:
       errorLabel = Label(topFrame, text=f'', font=('Lucida Bright', 10), bg='#81b29a', fg='red')
       errorLabel.grid(row=2, column=1, pady=15)
 
-      button = Button(topFrame, border=2, text='Get', font=('Lucida Bright', 13), relief='groove', bg='#fefae0', cursor="hand2", command=lambda: self.getData(monthEntry.get(), dayEntry.get(), yearEntry.get()))
-      button.grid(row=3, column=1, pady=5, ipadx=50, ipady=10)
+      button = Button(topFrame, border=2, text='Show graph', font=('Lucida Bright', 12), relief='groove', bg='#fefae0', cursor="hand2", command=lambda: self.getData(monthEntry.get(), dayEntry.get(), yearEntry.get()))
+      button.grid(row=3, column=1, pady=1, ipadx=30, ipady=5)
       button.configure(activebackground='#e9edc9')
 
       timesChargedLabel = Label(topFrame, text=f"", bg='#81b29a', fg='#fefae0', font=('San Serif', 10))
@@ -173,16 +206,63 @@ class AppGUI:
       avgBattLife = Label(topFrame, text=f"", bg='#81b29a', fg='#fefae0', font=('San Serif', 10))
       avgBattLife.grid(row=3, column=2)
       #######################################################################################################################
+      #records frame inputs and labels
+      
+      searchMonth = Entry(searchFrame, highlightthickness=1, border=2, font=('San Serif', 9), justify=CENTER)
+      searchMonth.grid(row=0, column=0, ipady=5, padx=10, pady=10)
+
+      searchYear = Entry(searchFrame, highlightthickness=1, border=2, font=('San Serif', 9), justify=CENTER)
+      searchYear.grid(row=0, column=1, ipady=5, padx=10, pady=10)
+
+      button = Button(searchFrame, border=2, text='Search', font=('San Serif', 11), relief='groove', bg='#fefae0', cursor="hand2", command=lambda: self.searchQuery(searchMonth.get(), searchYear.get()))
+      button.grid(row=1, column=0, pady=3, ipadx=15)
+      button.configure(activebackground='#e9edc9')
+
+      ###########################################Treeview#########################################################
+
+      tree_scroll = Scrollbar(dataFrame)
+      tree_scroll.pack(side=RIGHT, fill=Y)
+
+      style = ttk.Style()
+      style.theme_use("clam")  
+      style.configure("Treeview",
+         background="white",
+         foreground="black",
+         rowheight=25,
+         fieldbackground="white"
+         )
 
 
-      bottomFrame = Frame(mainFrame, relief='sunken', border=3, height=600)
-      bottomFrame.grid(row=1, column=0, ipadx=self.appWidth)
+      my_tree = ttk.Treeview(dataFrame, yscrollcommand=tree_scroll.set)
+      my_tree.pack(ipadx=50, ipady=165, fill=BOTH)
+
+      tree_scroll.config(command=my_tree.yview)
+
+      my_tree['columns'] = ("Day", "Year")
+
+      my_tree.column("#0", width=0, stretch=NO)
+      my_tree.column("Day", anchor=CENTER, width=100)
+      my_tree.column("Year", anchor=CENTER, width=120)
       
 
+      my_tree.heading("#0", text='', anchor=W)
+      my_tree.heading("Day", text="Day", anchor=CENTER)
+      my_tree.heading("Year", text="Year", anchor=CENTER)
+
+      ####################################################################################################
+      #bottom graph frame
+      bottomFrame = Frame(mainFrame, relief='sunken', border=3, height=600, bg='#ffffff', width=900)
+      bottomFrame.grid(row=1, column=1)
+      
+      my_tree.bind("<Double-1>", my_tree.focus())
+      searchFrame.grid_propagate(False)
+      dataFrame.grid_propagate(False)
       topFrame.grid_propagate(False)
       bottomFrame.grid_propagate(False)
 
+
 AppGUI()
+
 window.bind('<Button>', lambda event: event.widget.focus_set())
 window.resizable(False, False) 
 window.mainloop()
